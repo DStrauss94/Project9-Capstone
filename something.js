73872c10366bb44d8340a525d56034485e1cf4aa
Douglas Strauss <douglasstrauss94@gmail.com>
2022-01-17 20:03:05 -0800

    updated toggle button

--- something.js
+++ something.js
@@ -1,5 +1,9 @@
 
 function myFunction() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle('dark-mode');
+
+    // var darkCarousel = document.getElementsByClassName('carousel-item');
+    // darkCarousel.classList.toggle("dark-mode-carousel");
+    
  }
