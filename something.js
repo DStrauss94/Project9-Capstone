bf2ff8bc360d0b2bc9991e3cc0ccc02854e1aa5b
Douglas Strauss <douglasstrauss94@gmail.com>
2022-02-03 21:24:41 -0800

    understanding and implementing HTML/DOM interaction for Grid Layout

--- something.js
+++ something.js
@@ -1,11 +1,13 @@
-// if(window.innerWidth >=768){
-//     let list = document.getElementById("carouselExampleControls");
-//     list.style.display = 'none';
-//     gridLayout();
-// }
-// else{
-//     list.style.display = 'none';
-//   }
+//When the page loads, check to see if screen is equal or bigger than 768px
+//If so, then it removes Carousel Html
+document.addEventListener("DOMContentLoaded", function() { 
+    if(window.innerWidth >=768){
+        let list = document.getElementById("carouselExampleControls");
+        list.style.display = 'none';
+        gridLayout();
+    }
+}, true);
+
 
 
 toggleBody = () => {
@@ -35,24 +37,23 @@ toggleLines = () => {
 
 window.addEventListener('resize', () => {
     let list = document.getElementById("carouselExampleControls");
-    if(window.innerWidth <= 768){
+    let list2 = document.getElementById("grid-project");
+    if(window.innerWidth < 768){
       list.style.display = '';
+      list2.style.display = 'none';
     } else{
       list.style.display = 'none';
+      list2.style.display = '';  
+      
     }
+    
   });
 
+//where my Grid HTML will populate 
+ gridLayout = () => {
 
- 
-
-
-gridLayout = () => {
-    
-        const p = document.createElement("p");
-        p.innerHTML = "Hello World!";
-        let project1 = document.getElementById('grid-project');
-        project1.append(p);
-        
-        
-    
-}
+        const div = document.createElement("div");
+        const text = document.createTextNode("Form Project");
+        div.appendChild(text);
+        document.getElementById("grid-project").appendChild(div);
+    }
